e22c940a166499145e10b63fb526a42731fa401a
freeboardtortoise <darionkfitt@gmail.com>
2025-09-09 12:57:15 +0000

    added if statements

--- interpriter.py
+++ interpriter.py
@@ -1,11 +1,16 @@
-memory = [0] * 1000000
+import time
+memory = [0] * 10000000
 functions = []
+currentLine = 0
 def interprit(code, arg=None):
-    global functions, memory
+    global functions, memory, currentLine
     print("setting memory")
     if arg is not None:
         memory[int("100000",2)] = arg
-    for line in code.split("\n"):
+    newCode=code.split("\n")
+    while currentLine < len(newCode):
+        time.sleep(0.01)
+        line = newCode[currentLine]
         line = line.split(" ")
         print(line)
 
@@ -30,4 +35,27 @@ def interprit(code, arg=None):
             memory[int(line[1],2)] = memory[int(line[2],2)] / memory[int(line[3],2)]
         elif line[0] == "110001":
             functions.append([line[1],line[2:]])
+        currentLine+=1
 
+        if line[0][0] == "1": #if statements
+            print("if")
+            if line[0] == "100001":
+                print("==")
+                if memory[int(line[1], 2)] == memory[int(line[2], 2)]:
+                    currentLine=int(line[3], 2)
+                    print(f"goto {int(line[3], 2)}")
+            elif line[0] == "100010":
+                if memory[int(line[1],2)] >= memory[int(lint[2],2)]:
+                    currentLine=int(line[3], 2)
+            elif line[0] == "100011":
+                if memory[int(line[1],2)] <= memory[int(lint[2],2)]:
+                    currentLine=int(line[3], 2)
+            elif line[0] == "100110":
+                if memory[int(line[1],2)] > memory[int(lint[2],2)]:
+                    currentLine=int(line[3], 2)
+            elif line[0] == "100111":
+                if memory[int(line[1],2)] < memory[int(lint[2],2)]:
+                    currentLine=int(line[3], 2)
+            elif line[0] == "100101":
+                if memory[int(line[1],2)] != memory[int(lint[2],2)]:
+                    currentLine=int(line[3], 2)
